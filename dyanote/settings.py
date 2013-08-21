da2f2ade4e9bbec2ea8129d671224a846134c4d7
Matteo Nardi <commits@matteonardi.org>
2013-08-21 09:52:37 +0200

    Add AngularJS seed

--- dyanote/settings.py
+++ dyanote/settings.py
@@ -73,7 +73,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(PROJECT_ROOT), "static"),
+    os.path.join(os.path.dirname(PROJECT_ROOT), "client", "app"),
 )
 
 # List of finder classes that know how to find static files in
