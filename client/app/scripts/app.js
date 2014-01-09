1c8f1b9edae57cc1de52e3a0aa47461619c25fd3
Matteo Nardi <commits@matteonardi.org>
2014-01-09 11:19:19 +0100

    Upgrade AngularJS, add header template

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -7,6 +7,10 @@ angular.module('clientApp', ['ngRoute'])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/asd/', {
+        templateUrl: 'views/header.html',
+        controller: 'MainCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
