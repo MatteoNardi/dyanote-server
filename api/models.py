b0e39088d326557192486a24c87df3b68bf617ce
Matteo Nardi <commits@matteonardi.org>
2014-02-11 20:25:43 +0100

    Mark Page's parent field as 'blank'

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ class Page(models.Model):
     """A Page in Dyanote."""
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, default='')
-    parent = models.ForeignKey('api.Page', null=True, related_name='children')
+    parent = models.ForeignKey('api.Page', null=True, blank=True, related_name='children')
     body = models.TextField(blank=True, default='')
     author = models.ForeignKey('auth.User', related_name='pages')
 
